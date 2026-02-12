b29db3f6265fec25c0437782075c1f82a871cc16
brileco90-design <brileco90@gmail.com>
2026-02-13 00:34:43 +0100

    Primer commit: Catálogo y filtros de Alexa Moda

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Vestidos from "./pages/Vestidos";
+import Vestidos from "./pages/vestidos";
 import ProductDetail from "./pages/ProductDetail";
-import Carrito from "./pages/Carrito";
+import Carrito from "./pages/carrito";
 import Admin from "./pages/Admin";
 import NotFound from "./pages/NotFound";
 
